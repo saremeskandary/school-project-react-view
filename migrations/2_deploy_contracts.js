83f5adf2d8451992195d48b135b7df485bf25014
sarem eskandary <saremeskandary@gmail.com>
2021-11-11 11:50:09 +0330

    react added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const Users = artifacts.require("./Users.sol");
 const Certification = artifacts.require("./Certification.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(SimpleStorage);
   deployer.deploy(Users);
   deployer.deploy(Certification);
 };
