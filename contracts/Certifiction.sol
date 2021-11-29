fcb2b7b4b0f49bedd04ac6f8931cc0cd603de047
sarem eskandary <saremeskandary@gmail.com>
2021-11-30 00:29:32 +0330

    createStudent, createSchool and createCertification tests created

--- contracts/Certifiction.sol
+++ contracts/Certifiction.sol
@@ -1,64 +1,76 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.9 <0.9.0;
+pragma solidity ^0.8.9;
+
 // import "./Users.sol";
 
 // TODO add NatSpec for every parameters like below
 /// @title
 /// @author Sarem Eskandary
-/// @notice 
+/// @notice
 /// @dev
 contract Certification {
     ////////////////////   State variables   ////////////////////
-    address public  owner;
-    uint public     schoolCount;
-    uint public     studentCount;
+    address public owner;
+    uint256 public schoolCount;
+    uint256 public studentCount;
     ////////////////////   events     ////////////////////
     // event personAdded(User user);
-    event studentCreated(address indexed sender, string indexed name, string indexed IPFShash);
-    event schoolCreated(address indexed sender, string indexed name, string indexed IPFShash);
-    event certificateCreated(string indexed _courseName, address indexed school, address indexed student);
+    event studentCreated(
+        address indexed sender,
+        string indexed name,
+        string indexed IPFShash
+    );
+    event schoolCreated(
+        address indexed sender,
+        string indexed name,
+        string indexed IPFShash
+    );
+    event certificateCreated(string indexed _courseName);
     event courseCreated(address indexed sender);
-    
+
     ////////////////////   enums     ////////////////////
     // enum ourseState {passed, inProgress}
     ////////////////////   struct     ////////////////////
     struct School {
-        bytes32[]   certificateList;
-        uint        certificatePointer;
-        string      name;
-        string      IPFShash; // additional info
-        address     schoolOwner;
+        bytes32[] certificateList;
+        uint256 certificatePointer;
+        string name;
+        string IPFShash; // additional info
+        address schoolOwner;
     }
-    mapping (address=>School) schoolStruct;
+    mapping(address => School) public schoolStruct;
     address[] public schoolList;
 
     struct Student {
-        bytes32[]   certificateList;
-        uint        certificatePointer;
-        string      name;
-        string      IPFShash; // additional info
+        bytes32[] certificateList;
+        uint256 certificatePointer;
+        string name;
+        string IPFShash; // additional info
     }
-    mapping (address=>Student) studentStruct;
+
+    mapping(address => Student) public studentStruct;
     address[] public studentList;
 
     struct Course {
-        address[]   student;
-        uint        certificatePointer;
-        address     schoolID;
-        string      courseName;
-        string      IPFShash; // additional info
+        address[] students;
+        uint256 certificatePointer;
+        address schoolID;
+        string courseName;
+        string IPFShash; // additional info
     }
-    mapping (bytes32=>Course) courseStruct;
+
+    mapping(bytes32 => Course) public courseStruct;
     bytes32[] public courseList;
 
     struct Certificate {
-        uint        certificatePointer;
-        address     studentID;
-        address     schoolID;
-        bytes32     courseID;
-        string      courseName;
+        uint256 certificatePointer;
+        address studentID;
+        address schoolID;
+        bytes32 courseID;
+        string courseName;
     }
-    mapping (bytes32=>Certificate) certificateStruct;
+
+    mapping(bytes32 => Certificate) public certificateStruct;
     bytes32[] public certificateList;
 
     ////////////////////   modifiers  ////////////////////
@@ -71,7 +83,10 @@ contract Certification {
         _;
     }
     modifier isSchool(address _schoolOwner) {
-        require(msg.sender == schoolStruct[_schoolOwner].schoolOwner , "Must be the school to call this function");
+        require(
+            msg.sender == schoolStruct[_schoolOwner].schoolOwner,
+            "Must be the school to call this function"
+        );
         _;
     }
 
@@ -81,45 +96,124 @@ contract Certification {
     //     _;
     // }
 
-    constructor() {
-    }
+    constructor() {}
 
     /// @dev school should add student to the school list
-    function createStudent(string memory _name, string memory _IPFShash) public returns(bool success) {
+    function createStudent(string memory _name, string memory _IPFShash)
+        public
+        returns (bool success)
+    {
         studentList.push(msg.sender);
         studentStruct[msg.sender].certificatePointer = studentList.length - 1;
-        studentStruct[msg.sender].name               = _name;
-        studentStruct[msg.sender].IPFShash           = _IPFShash;
+        studentStruct[msg.sender].name = _name;
+        studentStruct[msg.sender].IPFShash = _IPFShash;
 
         emit studentCreated(msg.sender, _name, _IPFShash);
         return (true);
     }
-    function createSchool(string memory _name, string memory _IPFShash) public returns (bool success) {
+
+    function createSchool(string memory _name, string memory _IPFShash)
+        public
+        returns (bool success)
+    {
         schoolList.push(msg.sender);
         schoolStruct[msg.sender].certificatePointer = schoolList.length - 1;
-        schoolStruct[msg.sender].name               = _name;
-        schoolStruct[msg.sender].IPFShash           = _IPFShash;
+        schoolStruct[msg.sender].name = _name;
+        schoolStruct[msg.sender].IPFShash = _IPFShash;
+        schoolStruct[msg.sender].schoolOwner = msg.sender;
 
         emit schoolCreated(msg.sender, _name, _IPFShash);
         return (true);
     }
-    function createCourse(string memory _name, address _schoolID) 
-    public isSchool(_schoolID) returns (bool success) {
+
+    function createCourse(string memory _name)
+        public
+        isSchool(msg.sender)
+        returns (bool success)
+    {
         
     }
-    function createCertificate(bytes32 _ID, address _student, address _school, string memory _courseName) 
-    public isSchool(_school) returns (bool success) {
+
+    function createCertificate(
+        bytes32 _ID,
+        address _student,
+        string memory _courseName
+    ) public isSchool(msg.sender) returns (bool success) {
         certificateList.push(_ID);
         certificateStruct[_ID].certificatePointer = certificateList.length - 1;
-        certificateStruct[_ID].studentID          = _student;
-        certificateStruct[_ID].schoolID           = _school;  
-        certificateStruct[_ID].courseName         = _courseName;
+        certificateStruct[_ID].studentID = _student;
+        certificateStruct[_ID].schoolID = msg.sender;
+        certificateStruct[_ID].courseName = _courseName;
 
-        emit certificateCreated(_courseName, _school, _student);
+        emit certificateCreated(_courseName);
         return (true);
     }
-    // function fetchStudent(address _studentAddress) view public returns (type name) {
-        
-   // }
-   
+
+    function fetchSchool(address _schoolAddress)
+        public
+        view
+        returns (
+            uint256 certificatePointer,
+            string memory name,
+            string memory IPFShash,
+            address schoolOwner
+        )
+    {
+        certificatePointer = schoolStruct[_schoolAddress].certificatePointer;
+        name = schoolStruct[_schoolAddress].name;
+        IPFShash = schoolStruct[_schoolAddress].IPFShash;
+        schoolOwner = schoolStruct[_schoolAddress].schoolOwner;
+        return (certificatePointer, name, IPFShash, schoolOwner);
+    }
+
+    function fetchStudent(address _studentAddress)
+        public
+        view
+        returns (
+            uint256 certificatePointer,
+            string memory name,
+            string memory IPFShash
+        )
+    {
+        certificatePointer = studentStruct[_studentAddress].certificatePointer;
+        name = studentStruct[_studentAddress].name;
+        IPFShash = studentStruct[_studentAddress].IPFShash;
+        return (certificatePointer, name, IPFShash);
+    }
+
+    function fetchCertificate(bytes32 _certificate)
+        public
+        view
+        returns (
+            uint256 certificatePointer,
+            address studentID,
+            address schoolID,
+            bytes32 courseID,
+            string memory courseName
+        )
+    {
+        certificatePointer = certificateStruct[_certificate].certificatePointer;
+        studentID = certificateStruct[_certificate].studentID;
+        schoolID = certificateStruct[_certificate].schoolID;   
+        courseName = certificateStruct[_certificate].courseName;
+        return (certificatePointer, studentID, schoolID, courseID, courseName);
+    }
 }
+    // TODO fix this
+    // function fetchCourse(bytes32 _course)
+    //     public
+    //     view
+    //     returns (
+    //         address students,
+    //         uint256 certificatePointer,
+    //         address schoolID,
+    //         string memory courseName,
+    //         string memory IPFShash // additional info
+    //     )
+    // {
+    //     students = courseStruct[_course].students;
+    //     certificatePointer = courseStruct[_course].certificatePointer;
+    //     schoolID = courseStruct[_course].schoolID;
+    //     courseName = courseStruct[_course].courseName;
+    //     IPFShash = courseStruct[_course].IPFShash;
+    // }
--- test/certification.test.js
+++ test/certification.test.js
@@ -2,19 +2,22 @@ const Certification = artifacts.require("./Certification.sol");
 let BN = web3.utils.BN;
 let { catchRevert } = require("./exceptionsHelpers.js");
 const { items: ItemStruct, isDefined, isPayable, isType, isDefinedAndType } = require("./ast-helper");
+const { assert } = require("chai");
 
 contract("Certification", accounts => {
-  const [_owner, alice, bob] = accounts;
+  const [_owner, school, sarem, ali] = accounts;
+  const id = "0x6c00000000000000000000000000000000000000000000000000000000000000"
   const emptyAddress = "0x0000000000000000000000000000000000000000";
-  
-  const price = "1000";
-  const excessAmount = "2000";
-  const name = "book";
+  const nameSchool = "Sahar School";
+  const nameAli = "ali";
+  const nameCertificate = "first year";
+  const IPFShashSchool = "Sahar Image"
+  const IPFShashAli = "Sahar Image"
 
   let instance;
 
   beforeEach(async () => {
-    instance = await Certification.new();
+    instance = await Certification.deployed();
   })
 
   describe('deploiment', async () => {
@@ -25,11 +28,94 @@ contract("Certification", accounts => {
       assert.notEqual(address, null)
       assert.notEqual(address, undefined)
     })
+  })
 
-    // it('has a name', async () => {
-    //   const name = await instance.name()
-    //   assert.equal(name, 'Certification')
+  // variablesAndStructsTest()
+
+  describe('functions', () => {
+    describe('student fuctions', () => {
+      it('should add a student with name and IPFShash', async () => {
+        await instance.createStudent(nameAli, IPFShashAli, {from: ali});
+
+        const result = await instance.fetchStudent.call(ali)
+        
+        assert.equal(
+          result[1], nameAli,
+          "the name of the last added student does not match the expected value"
+        )
+        assert.equal(
+          result[2], IPFShashAli,
+          "the IPFShash of the last added student does not match the expected value"
+        )
+      })
+    })
+
+    describe('school', () => {
+      it('should add a school with name, IPFShash, certificatePointer', async () => {
+        await instance.createSchool(nameSchool, IPFShashSchool, {from: school});
+
+        const result = await instance.fetchSchool.call(school)
+        
+        assert.equal(
+          result[1], nameSchool,
+          "the name of the last added school does not match the expected value"
+        )
+        assert.equal(
+          result[2], IPFShashSchool,
+          "the IPFShash of the last added school does not match the expected value"
+          )
+        assert.equal(
+          result[3], school,
+          "the schoolOwner of the last added school does not match the expected value"
+        )
+      })
+    })
+
+    // describe('course functions', () => {
+    //   it('should add a schoolID, courseName, IPFShash', async () => {
+    //     await instance.createCourse(nameCourse, IPFShashCourse, {from: school});
+
+    //     const result = await instance.fetchCourse.call(school)
+        
+    //     assert.equal(
+    //       result[1], nameCourse,
+    //       "the name of the last added school does not match the expected value"
+    //     )
+    //     assert.equal(
+    //       result[2], IPFShashCourse,
+    //       "the IPFShash of the last added school does not match the expected value"
+    //       )
+    //     assert.equal(
+    //       result[3], school,
+    //       "the schoolOwner of the last added school does not match the expected value"
+    //     )
+    //   })
     // })
+    
+    describe('certificate functions', () => {
+      it('should add a schoolID, studentID, courseName, id', async () => {
+        await instance.createCertificate(id, ali, nameCertificate, {from: school});
+
+        const result = await instance.fetchCertificate.call(id)
+      
+        assert.equal(
+          result[1], ali,
+          "the ali of the last added certificate does not match the expected value"
+          )
+        assert.equal(
+          result[2], school,
+          "the id of the last added certificate does not match the expected value"
+        )
+        assert.equal(
+          result[4], nameCertificate,
+          "the nameCertificate of the last added certificate does not match the expected value"
+        )
+      })
+    })
+  })
+});
+
+function variablesAndStructsTest() {
   describe('variables', () => {
     it("have an owner", async () => {
       assert.equal(typeof instance.owner, 'function', "the contract has no owner");
@@ -45,130 +131,126 @@ contract("Certification", accounts => {
   })
 
   describe('structs', () => {
-    describe('School Struct', () => {
-    let School
+    describe('Course Struct', () => {
+      let School
 
-    before(async () => {
-      School = await ItemStruct(Certification, "School")
-      assert(
-        School != null,
-        "The contract should define an `School Struct`"
-      )
+      before(async () => {
+        School = await ItemStruct(Certification, "School")
+        assert(
+          School != null,
+          "The contract should define an `School Struct`"
+        )
+      })
+
+      // it('this is a test for certificateList', async () => {
+      //   isDefinedAndType(School)("certificateList")("bytes32[]")
+      // })
+
+      it('this is a test for certificatePointer', async () => {
+        isDefinedAndType(School)("certificatePointer")("uint256")
+      })
+
+      it('this is a test for name', async () => {
+        isDefinedAndType(School)("name")("string")
+      })
+
+      it('this is a test for IPFShash', async () => {
+        isDefinedAndType(School)("IPFShash")("string")
+      })
     })
 
-    it('this is a test for certificateList', async () => {
-      isDefinedAndType(School)("certificateList")("bytes32[]")
+    describe('Student Struct', () => {
+      let Student
+
+      before(async () => {
+        Student = await ItemStruct(Certification, "Student")
+        assert(
+          Student != null,
+          "The contract should define an `Student Struct`"
+        )
+      })
+
+      // it('this is a test for certificateList', async () => {
+      //   isDefinedAndType(Student)("certificateList")("bytes32[]")
+      // })
+
+      it('this is a test for certificatePointer', async () => {
+        isDefinedAndType(Student)("certificatePointer")("uint256")
+      })
+
+      it('this is a test for name', async () => {
+        isDefinedAndType(Student)("name")("string")
+      })
+
+      it('this is a test for IPFShash', async () => {
+        isDefinedAndType(Student)("IPFShash")("string")
+      })
     })
 
-    it('this is a test for certificatePointer', async () => {
-      isDefinedAndType(School)("certificatePointer")("uint")
+    describe('Course Struct', () => {
+      let Course
+
+      before(async () => {
+        Course = await ItemStruct(Certification, "Course")
+        assert(
+          Course != null,
+          "The contract should define an `Course Struct`"
+        )
+      })
+
+      // it('this is a test for students', async () => {
+      //   isDefinedAndType(Course)("students")("address")
+      // })
+
+      it('this is a test for certificatePointer', async () => {
+        isDefinedAndType(Course)("certificatePointer")("uint256")
+      })
+
+      it('this is a test for schoolID', async () => {
+        isDefinedAndType(Course)("schoolID")("address")
+      })
+
+      it('this is a test for courseName', async () => {
+        isDefinedAndType(Course)("courseName")("string")
+      })
+
+      it('this is a test for IPFShash', async () => {
+        isDefinedAndType(Course)("IPFShash")("string")
+      })
     })
 
-    it('this is a test for name', async () => {
-      isDefinedAndType(School)("name")("string")
-    })
+    describe('Certificate Struct', () => {
+      let Certificate
 
-    it('this is a test for IPFShash', async () => {
-      isDefinedAndType(School)("IPFShash")("string")
+      before(async () => {
+        Certificate = await ItemStruct(Certification, "Certificate")
+        assert(
+          Certificate != null,
+          "The contract should define an `Certificate Struct`"
+        )
+      })
+
+      it('this is a test for name', async () => {
+        isDefinedAndType(Certificate)("certificatePointer")("uint256")
+      })
+      
+      it('this is a test for studentID', async () => {
+        isDefinedAndType(Certificate)("studentID")("address")
+      })
+
+      it('this is a test for schoolID', async () => {
+        isDefinedAndType(Certificate)("schoolID")("address")
+      })
+
+      it('this is a test for id', async () => {
+        isDefinedAndType(Certificate)("id")("bytes32")
+      })
+
+      it('this is a test for courseName', async () => {
+        isDefinedAndType(Certificate)("courseName")("string")
+      })
     })
   })
+}
 
-  describe('Student Struct', () => {
-    let Student
-
-    before(async () => {
-      Student = await ItemStruct(Certification, "Student")
-      assert(
-        Student != null,
-        "The contract should define an `Student Struct`"
-      )
-    })
-
-    it('this is a test for certificateList', async () => {
-      isDefinedAndType(Student)("certificateList")("bytes32[]")
-    })
-
-    it('this is a test for certificatePointer', async () => {
-      isDefinedAndType(Student)("certificatePointer")("uint")
-    })
-
-    it('this is a test for name', async () => {
-      isDefinedAndType(Student)("name")("string")
-    })
-
-    it('this is a test for IPFShash', async () => {
-      isDefinedAndType(Student)("IPFShash")("string")
-    })
-  })
-
-  describe('Course Struct', () => {
-    let Course
-
-    before(async () => {
-      Course = await ItemStruct(Certification, "Course")
-      assert(
-        Course != null,
-        "The contract should define an `Course Struct`"
-      )
-    })
-
-    it('this is a test for student', async () => {
-      isDefinedAndType(Course)("student")("address")
-    })
-
-    it('this is a test for certificatePointer', async () => {
-      isDefinedAndType(Course)("certificatePointer")("uint")
-    })
-
-    it('this is a test for schoolID', async () => {
-      isDefinedAndType(Course)("schoolID")("address")
-    })
-
-    it('this is a test for courseName', async () => {
-      isDefinedAndType(Course)("courseName")("string")
-    })
-
-    it('this is a test for IPFShash', async () => {
-      isDefinedAndType(Course)("IPFShash")("string")
-    })
-  })
-
-  describe('Certificate Struct', () => {
-    let Certificate
-
-    before(async () => {
-      Certificate = await ItemStruct(Certification, "Certificate")
-      assert(
-        Certificate != null,
-        "The contract should define an `Certificate Struct`"
-      )
-    })
-
-    it('this is a test for name', async () => {
-      isDefinedAndType(Certificate)("certificatePointer")("uint")
-    })
-    
-    it('this is a test for studentID', async () => {
-      isDefinedAndType(Certificate)("studentID")("address")
-    })
-
-    it('this is a test for schoolID', async () => {
-      isDefinedAndType(Certificate)("schoolID")("address")
-    })
-
-    it('this is a test for courseID', async () => {
-      isDefinedAndType(Certificate)("courseID")("bytes32")
-    })
-
-    it('this is a test for courseName', async () => {
-      isDefinedAndType(Certificate)("courseName")("string")
-    })
-  })
-  })
   
-  describe('Use cases', () => {
-    
-  })
-  
-  })
-});
