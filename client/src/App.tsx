75bd43cfefaad08de61a91068425ae6ce2fb123c
sarem eskandary <saremeskandary@gmail.com>
2021-12-07 03:04:52 +0330

    Bootstrap styling added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,66 +2,66 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Web3 from 'web3';
 import w3utils from 'web3-utils'
 import { networks, abi } from "./contracts/Certification.json";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Container, Form, Button, FloatingLabel } from 'react-bootstrap';
 
 import './App.css';
 
-function App() {
+export default function App() {
   const name = useRef<string>('')
   const image = useRef<string>('')
   const useCase = useRef<string>('')
   const initialize = useRef<boolean>(false)
   const [account, setAccount] = useState<string>('')
   const [myContract, setMyContract] = useState<any>()
-  const [submited, setSubmited] = useState<boolean>(false)
   const [isNeedSchool, setIsNeedSchool] = useState<boolean>(false)
-
+  const [validated, setValidated] = useState(false);
   const firstRender = useRef(false);
-  useEffect(() => {
-    async function init() {
-      let provider = window.ethereum
-      if (provider) {
-        window.web3 = new Web3(provider);
-        await provider.request({ method: 'eth_requestAccounts' })
-        console.log('new provider');
-      } else if (window.web3) {
-        window.web3 = new Web3(window.web3.currentProvider)
-        console.log('currentProvider');
-      }
-      else {
-        window.alert('non ethereum browser detected. You should consider to trying metamask')
-      }
-
-      const web3 = new Web3(provider)
-      let accounts = await web3.eth.getAccounts()
-      setAccount(accounts[0])
-      window.ethereum.on("accountsChanged", async function () {
-        // Time to reload your interface with accounts[0]!
-        accounts = await web3.eth.getAccounts();
-        // accounts = await web3.eth.getAccounts();
-        setAccount(accounts[0])
-        console.log(accounts);
-      });
-      console.log(account);
-      let networkId = Object.keys(networks)[0] as keyof typeof networks;
-      const networkData = networks[networkId]
-      if (networkData) {
-        setMyContract(new web3.eth.Contract(abi as w3utils.AbiItem[], networks[networkId].address))
-        console.log('contract created');
-      } else {
-        window.alert('certification dapp not deployed in this network')
-      }
-      window.ethereum.on('chainChanged', function (_chainId: any) {
-        networkData === _chainId ? window.location.reload()
-          : window.alert('certification dapp not deployed in this network')
-      });
-      initialize.current = true
+  async function init() {
+    let provider = window.ethereum
+    if (provider) {
+      window.web3 = new Web3(provider);
+      await provider.request({ method: 'eth_requestAccounts' })
+      console.log('new provider');
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+      console.log('currentProvider');
     }
+    else {
+      window.alert('non ethereum browser detected. You should consider to trying metamask')
+    }
+
+    const web3 = new Web3(provider)
+    let accounts = await web3.eth.getAccounts()
+    setAccount(accounts[0])
+    window.ethereum.on("accountsChanged", async function () {
+      // Time to reload your interface with accounts[0]!
+      accounts = await web3.eth.getAccounts();
+      // accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0])
+      console.log(accounts);
+    });
+    console.log(account);
+    let networkId = Object.keys(networks)[0] as keyof typeof networks;
+    const networkData = networks[networkId]
+    if (networkData) {
+      setMyContract(new web3.eth.Contract(abi as w3utils.AbiItem[], networks[networkId].address))
+      console.log('contract created');
+    } else {
+      window.alert('certification dapp not deployed in this network')
+    }
+    window.ethereum.on('chainChanged', function (_chainId: any) {
+      networkData === _chainId ? window.location.reload()
+        : window.alert('certification dapp not deployed in this network')
+    });
+    initialize.current = true
+  }
+  useEffect(() => {
     if (!initialize.current) { init() }
     if (firstRender) {
       fetchData()
     }
   })
-
 
   const fetchData = useCallback(() => {
     async function createStudent(name: string, IPFShash: string) {
@@ -118,61 +118,73 @@ function App() {
         console.error(err);
       }
     }
-    if (submited) {
+    if (validated) {
       fetchToWeb3().catch(error => console.error(error))
-      setSubmited(false)
+      setValidated(false)
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [submited])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [validated])
 
-  function handleSubmit(e: any) {
-    e.preventDefault()
-    setSubmited(true)
-    useCase.current = e.target.useCase.value
-    name.current = e.target.name.value
-    image.current = e.target.image.value
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+
+      event.stopPropagation();
+    }
+    console.log("Validated", useCase.current, name.current, image.current);
+
+    setValidated(true);
+    useCase.current = event.target.useCase.value
+    name.current = event.target.name.value
+    image.current = event.target.image.value
   }
 
 
   return (
     <div className="App">
-      <nav>
-        <h1>{account}</h1>
-        {isNeedSchool ? <h2>you need to be a school to create course or certificate</h2> : null}
-      </nav>
-      <form onSubmit={handleSubmit}>
-        <select
-          required id="useCase"
-          onChange={(e) => {
-            e.target.value === "course" || e.target.value === "certificate" ? setIsNeedSchool(true) : setIsNeedSchool(false)
-          }}
-        >
-          <option value="chose">chose</option>
-          <option value="student">student</option>
-          <option value="school">school</option>
-          <option value="course">course</option>
-          <option value="certificate">certificate</option>
-        </select>
-        <label>
-          Name:
-          <input
-            type="text"
-            required id="name"
-          />
-        </label>
-        <label>
-          additional info like image IPFS:
-          <input
-            type="text"
-            required id="image"
-          />
-        </label>
-        <input type="submit" value="Submit" onSubmit={() => {
-          console.log('afds');
-        }} />
-      </form>
+      <Container fluid="md">
+        <Nav className="justify-content-start" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              your address is : {account}
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <FloatingLabel label="what do you wana create?">
+            <Form.Select
+              aria-label="Floating label select example"
+              required
+              id = "useCase"
+              onChange={(e) => {
+                console.log(e.target.value)
+                e.target.value === "course" || e.target.value === "certificate" ? setIsNeedSchool(true) : setIsNeedSchool(false)
+              }}
+            >
+              <option >Open menu</option>
+              <option value="student">student</option>
+              <option value="school">school</option>
+              <option value="course">course</option>
+              <option value="certificate">certificate</option>
+            </Form.Select>
+          </FloatingLabel>
+          <br />
+
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>name</Form.Label>
+            <Form.Control type="name" placeholder="Enter name" />
+            {isNeedSchool ? <Form.Text style={{ color: "red" }}>note that you need to be a school to create a course or certificate</Form.Text> : null}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="image">
+            <Form.Label>image</Form.Label>
+            <Form.Control type="name" placeholder="Enter image" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
-
-export default App;
