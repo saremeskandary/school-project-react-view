d7f9873bfc0fb89769656eed6dac7c300968001d
sarem eskandary <saremeskandary@gmail.com>
2021-12-07 03:23:50 +0330

    expo client added

--- truffle-config.js
+++ truffle-config.js
@@ -4,6 +4,7 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "expo-client/src/contracts"),
   networks: {
     local: {
       host: "localhost",
